feec3cebb8957d7da1a94b7e50596cb42165c3dd
PedroPini <augustopini@gmail.com>
2019-11-09 19:12:09 -0300

    Adding new functionalities to the project

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,12 @@
+genesis_block = {
+    'previous_hash': '',
+    'index': 0,
+    'transactions': []
+}
+    
 blockchain = []
 open_transactions = []
+owner = 'Pedro'
 
 def get_last_blockchain_value():
     if len(blockchain) < 1:
@@ -7,24 +14,37 @@ def get_last_blockchain_value():
     return blockchain[-1]
 
 
-def add_transaction(sender, recipient, amount = 1.0):
-    """ Append a new value as well as the last blockchain value to the blockchain\n
-    Arguments:\n
-        :sender: The amount that should be added. \n
-        :last_transaction: The last blockchain transaction (default [1]).    
+def add_transaction(recipient, sender=owner, amount=1.0):
+    """ Append a new value as well as the last blockchain value to the blockchain
+
+    Arguments:
+        :sender: The sender of the coins.
+        :recipient: The recipient of the coins.
+        :amount: The amount of coins sent with the transaction (default = 1.0)
     """
-    if last_transaction == None:
-        last_transaction = [1]
+    transaction = {
+        'sender': sender,
+        'recipient': recipient,
+        'amount': amount
+    }
+    open_transactions.append(transaction)
     
     
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    block = {
+        'previous_hash': 'XYZ', 
+        'index': len(blockchain), 
+        'transactions': open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
     """Returns the input of the user (a new transaction amount) as a float"""
-    user_input = float(input('Your transaction amount please: '))
-    return user_input
+    tx_recipient = input('Enter the recipient of the transaction: ')
+    tx_amount = float(input('Your transaction amount please: '))
+    return tx_recipient, tx_amount
 
 def get_user_choice():
     user_input = input('Your choice: ')
@@ -61,8 +81,10 @@ while waiting_for_input:
     print('q: Quit')
     user_choice =get_user_choice()
     if user_choice == '1':
-        tx_amount = get_transaction_value()    
-        add_transaction(tx_amount, get_last_blockchain_value())
+        tx_data = get_transaction_value()   
+        recipient, amount = tx_data
+        add_transaction(recipient, amount = amount)
+        print(open_transactions)
     elif user_choice == '2':
         print_blockchain_elements()
     elif user_choice == 'h':
